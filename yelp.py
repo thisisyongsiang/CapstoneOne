9e9f16e87022ca693743689ee8ae32532b3a3d21
thisisyongsiang <73749752+thisisyongsiang@users.noreply.github.com>
2022-06-22 11:24:06 +0800

    yelp api access

---
+++ yelp.py
@@ -0,0 +1,36 @@
+import requests
+import json
+
+key="jbVyRyrgR8rFfBUlTBHzdapsZoi1RMIxkWpKbhzSDrxdHQlckHn2qm3UYafILzK75SATVm2nGv6ypJzdwfOKy2TaZ5nRP4RfsQkf41yR-V9QOPoGopzK8feCtEGxYnYx"
+lat,long=1.35644,103.83297
+url='https://api.yelp.com/v3'
+extension='/businesses/search'
+header={
+    "Authorization": "Bearer "+key
+}
+params={
+    "term":"food",
+    "latitude":lat,
+    "longitude":long,
+    "radius":10000,
+    "limit":50,
+    "sort_by":"distance",
+    "offset":0
+}
+total=9999
+dataCount=0
+overallObj=[]
+while dataCount<1000 and dataCount<total:
+    res=requests.get(url+extension,params=params,headers=header)
+    jsonObj=json.loads(res.text)
+    overallObj+=(jsonObj['businesses'])
+    if total==9999:
+        total=jsonObj['total']
+    print(jsonObj['total'])
+    print(dataCount)
+    params["offset"]+=len(jsonObj['businesses'])
+    dataCount+=len(jsonObj['businesses'])
+print(total)
+with open("yelpAPIData2.json",mode='w',encoding='utf-8') as f:
+    json.dump(overallObj, f, ensure_ascii=False, indent=4)
+print(jsonObj['total'])
