9a86456fa9f6c76f373965e90820ca21431bf47f
Salomone <darsalomone@gmail.com>
2022-06-30 23:16:59 +0800

    updated probability rating model

--- ProbabilityRatingModel.py
+++ ProbabilityRatingModel.py
@@ -17,5 +17,6 @@ def exponential(review_count, rating):
     Function takes in review and rating as inputs, and computes a consolidated rating value based on the confidence of the rating achieved by the number of reviews availability
     This uses an EXPONENTIAL probability model.
     """
+    
 
     return abs(review_count/(sqrt((1/0.09)*(review_count**2)+1000000)) + 0.7)*rating
