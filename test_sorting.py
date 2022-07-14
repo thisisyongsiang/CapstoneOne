63314f9015514ff0120cbaa687c236dc0dc61dd8
Salomone <darsalomone@gmail.com>
2022-07-14 15:07:06 +0800

    updated syntax for sort algo calls

--- test_sorting.py
+++ test_sorting.py
@@ -3,23 +3,26 @@ import Heapsort
 import QuickSelect
 import json
 import time
+import CategoryAndFilter as caf
 
 # Get and Sort Results from Yelp API
-dir="yelpAPIData.json"
+dir="/Users/robertsalomone/Downloads/singaporeFnBAll_final2.json"
 f=open(dir,encoding='utf-8')
 data=json.load(f)
 
-# QuickSelect
+data = caf.simplifyData(data, [1.397427, 103.881539], 1, 1, 1)
+
+# # QuickSelect
 start_time = time.time()
-firstN = QuickSelect.getFirstN(data, 'distance', 5, False)
+firstN = QuickSelect.QuickSelect(data, 'distance', False).GetNextN(5)
 print("QuickSelect - Time taken = {:.20f} seconds".format(time.time() - start_time))
 
 # MergeSort
 start_time = time.time()
-firstN = MergeSort.getFirstN(data, 'distance', 5, False)
+firstN = MergeSort.MergeSort(data, 'popularity', False).GetNextN(5)
 print("MergeSort - Time taken = {:.20f} seconds".format(time.time() - start_time))
 
 # HeapSort
 start_time = time.time()
-firstN = Heapsort.getItemsByField(data, 'distance', False).getTopN(5)
+firstN = Heapsort.getItemsByField(data, 'distance', False).getNextN(5)
 print("HeapSort - Time taken = {:.20f} seconds".format(time.time() - start_time))
