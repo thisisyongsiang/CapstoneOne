bea4a2b1bbcd6d6aca22177b672bf6c82e381811
Salomone <darsalomone@gmail.com>
2022-07-15 22:50:22 +0800

    updates test file

--- test.py
+++ test.py
@@ -14,10 +14,10 @@ f=open(dir,encoding='utf-8')
 data=json.load(f)
 
 simpleData=ct.simplifyData(data,[1.2872977, 103.8339319
-],1,2,3)
-clean_data = ct.getMultipleFoodCategories(simpleData, ['american restaurant'])
+],3,1,2)
+clean_data = ct.getMultipleFoodCategories(simpleData, ['italian restaurant'])
 clean_data = ct.filterDataByFieldAndValueRange(clean_data, 'price', [3,4])
-clean_data = ct.filterDataByFieldAndValueRange(clean_data, 'distance', [5600,10000])
+clean_data = ct.filterDataByFieldAndValueRange(clean_data, 'distance', [6000,10000])
 
 full_sorted_data = Heapsort.getItemsByField(clean_data, "recommendation", False)
 print(len(clean_data))
@@ -27,18 +27,44 @@ print(len(clean_data))
 # st.session_state.full_sorted_data = Heapsort.getItemsByField(sesh_state_clean_data, "recommendation", False)
 
 
-top_n = full_sorted_data.getNextN(2)
+# top_n = full_sorted_data.getNextN(5)
 
-print("top 7")
-print([top_n[i]['name'] for i in range(len(top_n))])
+next_output = []
+prev_output = []
+for i in range(len(clean_data)//5+1):
+    top_n = full_sorted_data.getNextN(5)
+    print("loop {}".format(i))
+    print([top_n[i]['name'] for i in range(len(top_n))])
 
-top_n2 = full_sorted_data.getNextN(2)
-print("next 7")
-print([top_n2[i]['name'] for i in range(len(top_n2))])
+    if i > len(clean_data)//5-4 and i < len(clean_data):
+        next_output += [top_n[i]['name'] for i in range(len(top_n))]
 
-prev_n = full_sorted_data.getPrevN(2)
-print("prev5")
-print([prev_n[i]['name'] for i in range(len(prev_n))])
+for i in range(3):
+    prev_n = full_sorted_data.getPrevN(5)
+    print("loop {}".format(i))
+    print([prev_n[i]['name'] for i in range(len(prev_n))])
+    prev_output += [prev_n[i]['name'] for i in range(len(prev_n))]
+
+discrep = False
+for i in range(len(prev_output)):
+    if prev_output[i] not in next_output:
+        print("DISCREP")
+        discrep = True
+
+if not discrep:
+    print("ALL GOOD")
+
+
+# print("top 5")
+# print([top_n[i]['name'] for i in range(len(top_n))])
+
+# top_n2 = full_sorted_data.getNextN(5)
+# print("next 5")
+# print([top_n2[i]['name'] for i in range(len(top_n2))])
+
+# prev_n = full_sorted_data.getPrevN(5)
+# print("prev5")
+# print([prev_n[i]['name'] for i in range(len(prev_n))])
 
 
 # session_top_n = st.session_state.full_sorted_data.getNextN(5)
